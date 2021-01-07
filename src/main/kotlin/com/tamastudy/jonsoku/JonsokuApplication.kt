7cb338a83e968fbc4a10b9be639f81511529baa0
jonsoku2 <whdtjr2792@gmail.com>
2021-01-07 21:09:00 +0900

    init project

---
+++ src/main/kotlin/com/tamastudy/jonsoku/JonsokuApplication.kt
@@ -0,0 +1,11 @@
+package com.tamastudy.jonsoku
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JonsokuApplication
+
+fun main(args: Array<String>) {
+    runApplication<JonsokuApplication>(*args)
+}
